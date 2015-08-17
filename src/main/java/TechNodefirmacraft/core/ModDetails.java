e0336b3633f9cf8b8fbb8df215b8f8c0fd877271
AnodeCathode <shotorius_dt@hotmail.com>
2015-08-16 20:26:30 -0600

    Oredic change for TFC bricks

--- src/main/java/TechNodefirmacraft/core/ModDetails.java
+++ src/main/java/TechNodefirmacraft/core/ModDetails.java
@@ -7,7 +7,7 @@ public class ModDetails
 
 	public static final int VersionMajor = 2;
 	public static final int VersionMinor = 0;
-	public static final int VersionRevision = 1;
+	public static final int VersionRevision = 2;
 
 	public static final String ModVersion = VersionMajor + "." + VersionMinor + "." + VersionRevision;
 	public static final String ModDependencies = "after:TerraFirmaCraft";
