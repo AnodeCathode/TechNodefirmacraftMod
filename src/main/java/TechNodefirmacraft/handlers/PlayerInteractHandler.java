c84ce1854fc2e68a1192973a97f53843644aacf1
Bunsan <bunsan@users.noreply.github.com>
2016-03-10 10:38:50 -0600

    Prevent JerryCan interaction with barrels. Remove dupe bug.

--- src/main/java/TechNodefirmacraft/handlers/PlayerInteractHandler.java
+++ src/main/java/TechNodefirmacraft/handlers/PlayerInteractHandler.java
@@ -37,7 +37,9 @@ public class PlayerInteractHandler {
 			Block target = event.world.getBlock(event.x, event.y, event.z);
 			if (target == TFCBlocks.barrel || target == TFCBlocks.vessel) {
 				String name = itemInHand.getUnlocalizedName();
-				if (name.contains("tile.openblocks.tank") || name.contains("tile.MachineBlock2.PortableTank") || name.contains("ImmersiveEngineering.drill") || name.contains("ImmersiveEngineering.chemthrower")){
+				if (name.contains("tile.openblocks.tank") || name.contains("tile.MachineBlock2.PortableTank") ||
+						name.contains("ImmersiveEngineering.drill") || name.contains("ImmersiveEngineering.chemthrower")
+						|| name.contains("ImmersiveEngineering.jerrycan")){
 					event.setCanceled(true);
 					return;
 				}
