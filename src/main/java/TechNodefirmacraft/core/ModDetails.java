56da49fb8e0b009e9da19941c9dc63ae83304568
Bunsan <bunsan@users.noreply.github.com>
2016-03-14 21:26:37 -0500

    Version and Forge Bump

--- src/main/java/TechNodefirmacraft/core/ModDetails.java
+++ src/main/java/TechNodefirmacraft/core/ModDetails.java
@@ -7,7 +7,7 @@ public class ModDetails
 	public static final String ModName = "TechNodefirmacraft";
 
 	public static final int VersionMajor = 2;
-	public static final int VersionMinor = 15;
+	public static final int VersionMinor = 16;
 	public static final int VersionRevision = 0;
 
 	public static final String ModVersion = VersionMajor + "." + VersionMinor + "." + VersionRevision;
