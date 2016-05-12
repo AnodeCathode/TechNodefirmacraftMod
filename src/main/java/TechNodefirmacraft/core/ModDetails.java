890e6a12d809a242b634a1c46f8a7f3c2111bc7e
Bunsan <bunsan@users.noreply.github.com>
2016-05-11 22:42:29 -0500

    Version bump

--- src/main/java/TechNodefirmacraft/core/ModDetails.java
+++ src/main/java/TechNodefirmacraft/core/ModDetails.java
@@ -8,7 +8,7 @@ public class ModDetails
 
 	public static final int VersionMajor = 2;
 	public static final int VersionMinor = 16;
-	public static final int VersionRevision = 3;
+	public static final int VersionRevision = 4;
 
 	public static final String ModVersion = VersionMajor + "." + VersionMinor + "." + VersionRevision;
 
