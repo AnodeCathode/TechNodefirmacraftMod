b2c8532da61c7cb24d9e12a67535effebd8393b4
AnodeCathode <shotorius_dt@hotmail.com>
2016-07-07 01:53:35 -0600

    Harder recipe for beacon

--- src/main/java/TechNodefirmacraft/core/ModRecipes.java
+++ src/main/java/TechNodefirmacraft/core/ModRecipes.java
@@ -59,7 +59,7 @@ public class ModRecipes
 	
 	private static void registerRecipes()
 	{
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.TFCBeacon, 1), "222", "232", "111", '1', "blockPlatinum", '2', "blockGlass", '3', "itemNetherStar"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.TFCBeacon, 1), "2 4", "535", "111", '1', "blockPlatinum", '2', "craftingToolHardChisel", '3', "itemNetherStar", '4', "craftingToolHardHammer", '5', "blockGlass"));
 	}
 
 
